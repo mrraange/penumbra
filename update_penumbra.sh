3b5cf039a0a9865b6b376ea8ac7ffc7421b4d171
Range <100018176+mrraange@users.noreply.github.com>
2022-09-06 22:50:14 +0300

    Update update_penumbra.sh

--- update_penumbra.sh
+++ update_penumbra.sh
@@ -17,7 +17,7 @@ sleep 2
 
 
 function line {
-  echo "---31.08.2022---------------------------------------------------------------------"
+  echo "---06.09.2022---------------------------------------------------------------------"
 }
 
 function colors {
@@ -33,7 +33,7 @@ function install_pen {
   git clone https://github.com/penumbra-zone/penumbra
   cd $HOME/penumbra
   git fetch
-  git checkout 027-thyone
+  git checkout 028-harpalyke
   cargo update
   cargo build --quiet --release --bin pcli
 }
